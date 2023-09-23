a485bb6f88c0f77328bfa30adcded223ec4a502c
DivyanshSingh8899 <145823663+DivyanshSingh8899@users.noreply.github.com>
2023-09-23 14:56:43 +0530

    feature added multiplication.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@ x=eval(input("Enter the first number:"))
 y=eval(input("Enter the second number:"))
 sum=x+y
 difference=x-y
+multiplication=x*y
 print(sum)
 print(difference)
+print(multiplication)
 print("This is a calculator")
